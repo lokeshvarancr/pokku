900d3e5928e1501e2945c8512cc401dc6dfeef94
Rajeshvaran <rajeshvara1991@gmail.com>
2023-11-19 00:02:09 +0530

    Initial commit

---
+++ pokku/src/main/java/com/example/pokku/ServletInitializer.java
@@ -0,0 +1,13 @@
+package com.example.pokku;
+
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+
+public class ServletInitializer extends SpringBootServletInitializer {
+
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+		return application.sources(PokkuApplication.class);
+	}
+
+}
---
+++ pokku/src/test/java/com/example/pokku/PokkuApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.pokku;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PokkuApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
